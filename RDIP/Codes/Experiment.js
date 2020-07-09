9334257b11e4d6f7e480b47cd093bf4d34cc0dfa
Rohitha <rohithagorantla28@gmail.com>
2020-07-09 22:22:46 +0530

    making lan selection drop down

--- RDIP/Codes/Experiment.js
+++ RDIP/Codes/Experiment.js
@@ -1,11 +1,6 @@
-﻿function display()
-{
+﻿
+function display() {
     document.getElementById("dis_sen").innerHTML = "Form a sentence (Declarative or Interrogative or any other type) from the given words";
     document.getElementById("dis_sub").innerHTML = "(select the buttons in proper order)";
 
 }
-
-
-
-
-
